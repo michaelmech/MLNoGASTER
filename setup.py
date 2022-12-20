bf7d9e3cfc095a30e502567414ebbc0f9774a8f6
michaelmech <56457392+michaelmech@users.noreply.github.com>
2022-12-19 22:23:07 -0800

    Update setup.py
    added editdistance to required imports

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1.0',
     description='machine learning network of genetic algorithms simulating topologically evolved representations',
     author='michaelmech',
-    install_requires=['gplearn','tpot','dask_ml','distributed','pygad','sklego']
+    install_requires=['gplearn','tpot','dask_ml','distributed','pygad','sklego','editdistance']
 )
 
 
